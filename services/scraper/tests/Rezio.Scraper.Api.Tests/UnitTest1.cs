faed1c2f657e2ce8cd004e975e7ee39c725ebcf0
Dariusz Handzlik <handzlik@outlook.com>
2026-07-17 01:16:21 +0200

    chore: scaffold scraper service skeleton

---
+++ services/scraper/tests/Rezio.Scraper.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Rezio.Scraper.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ services/scraper/tests/Rezio.Scraper.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Rezio.Scraper.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
