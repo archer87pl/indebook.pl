f22617a3b7bc1c732946974f8c278291e8659d5d
Dariusz Handzlik <handzlik@outlook.com>
2026-07-17 07:38:15 +0200

    chore: scaffold channel-sync service skeleton

---
+++ services/channelsync/tests/Rezio.ChannelSync.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Rezio.ChannelSync.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ services/channelsync/tests/Rezio.ChannelSync.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Rezio.ChannelSync.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
